4b8becbed17782d91d28ac962f0767e02d042de9
Mkdenno <dennis.kiprop@student.moringaschool.com>
2022-09-06 17:13:35 +0300

    It will add home page

---
+++ emergencyclient/src/components/Home.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import NavBar from "./NavBar";
+import Footer from "./Footer";
+import Reports from "./Reports";
+
+const Home = ({report,onAddReport}) => {
+
+  // let [visible, setButtonVisibility] = useState("");
