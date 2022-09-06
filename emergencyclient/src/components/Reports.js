cf9fa7b6b4205a9ffea74712a12a6b285ee1b830
Mkdenno <dennis.kiprop@student.moringaschool.com>
2022-09-06 17:13:06 +0300

    It will add report page

--- emergencyclient/src/components/Reports.js
+++ emergencyclient/src/components/Reports.js
@@ -14,4 +14,44 @@ const Reports = ({ report, params, onAddReport }) => {
     <span className="btn2"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></span></td>
   </tr>
   ));
+  // console.log(params)
 
+  // function handleHideShow(){
+  // if(isHide){
+  //   setIsHide('')
+  // }
+  // else{
+  //   setIsHide(<NewReportForm params={params}/>)
+  // }}
+
+  return (
+    <div>
+      <div className="report-container">
+        <div className="reportdislay">
+          <h3>Reports display</h3>
+          <table>
+            <tr>
+              <th>Date&Time</th>
+              <th>Title</th>
+              <th>Description</th>
+              <th>Location</th>
+              <th>Action</th>
+            </tr>
+            {reportdisplay}
+          </table>
+        </div>
+        <div className="fomreport">
+          <div>
+            <h3>Report an emergency</h3>
+          </div>
+
+          <button className="hideshow">Show/hide new Report form</button>
+          {/* {true ? [isHide] : null} */}
+          <NewReportForm params={params} onAddReport={onAddReport} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Reports;
