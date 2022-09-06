3902845e14257fcefec1729054cb9b067e668cfe
Mkdenno <dennis.kiprop@student.moringaschool.com>
2022-09-06 17:17:27 +0300

    It will add Inputs to NewReportForm component

---
+++ emergencyclient/src/components/NewReportForm.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+
+const NewReportForm = ({ params,onAddReport }) => {
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit} className="formR">
+        <input
+          type="text"
+          name="title"
+          placeholder="Enter Type of emergency report"
+          onChange={handleInputChange}
+          value={inputData.title}
+        />
+        <br />
+        <input
+          type="text"
+          name="description"
+          placeholder="Enter Description of emergency report"
+          onChange={handleInputDescription}
+          value={inputData.description}
+        />
+        <br />
+        <input
+          type="text"
+          name="location"
+          placeholder="Enter Location of emergency report"
+          onChange={handleInputLocation}
+          value={inputData.location}
+        />
+        <br />
+        <button type="submit" className="report-btn">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default NewReportForm;
