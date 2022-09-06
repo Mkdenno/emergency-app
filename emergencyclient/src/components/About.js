db3e6dcad406f3b0cb288d10197ec5edd9c77774
Mkdenno <dennis.kiprop@student.moringaschool.com>
2022-09-06 17:15:50 +0300

    It will add Images to about component

--- emergencyclient/src/components/About.js
+++ emergencyclient/src/components/About.js
@@ -5,3 +5,14 @@ const About = () => {
   return (
     <div>
         <NavBar/>
+        <div className="ambulencedescription">
+      <div className="description">
+        <h2>Ambulance</h2>
+        <p>vehicle equipped to provide emergency care to sick or injured people and to get them to hospital.
+           Ambulances can also be used to transport patients between hospitals. There are several types of ambulance</p>
+
+      </div>
+
+}
+
+export default About
