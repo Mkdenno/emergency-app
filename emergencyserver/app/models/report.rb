2ef2f0032026e5139787aae3c7dc72283beaddbb
Mkdenno <dennis.kiprop@student.moringaschool.com>
2022-09-12 01:39:59 +0300

    Done

--- emergencyserver/app/models/report.rb
+++
@@ -1,7 +0,0 @@
-class Report < ActiveRecord::Base
-    belongs_to :user
-
-    # def cast_role(title, description)
-    #     self.create(title:title, description:description, user_id:self.id )
-    #   end
-end
